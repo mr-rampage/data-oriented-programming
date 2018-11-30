648fd8fad5cb60552fd84c7e640a557c56c88a85
mr-rampage <readonly@che.wbac.ca>
2018-11-29 23:53:30 -0500

    feat(multiple explosions)

--- js/app.js
+++ js/app.js
@@ -1,28 +1,43 @@
-const canvas = document.getElementById('particleCanvas');
-const pixelRenderer = render.bind(null, canvas.getContext('2d'));
+const container = document.querySelector('#particleContainer');
 
-canvas.addEventListener('click', (e) =>
-  pixelRenderer(
-    seed(1000, cursorPosition(canvas, e)),
-    createPixelImage(2, randomColour())
-  )
-);
+container.addEventListener('click', (e) => {
+    const canvas = container.appendChild(particleLayer(container));
+    render(
+        canvas,
+        seed(500, cursorPosition(canvas, e)),
+        createPixelImage(2, randomColour())
+    );
+});
 
-function seed(numberOfParticles, root) {
-  return Array(numberOfParticles)
-    .fill(null)
-    .map(() => {
-      const direction = unitVector();
+function particleLayer(container) {
+    const width = document.createAttribute('width');
+    width.value = container.offsetWidth;
 
-      return particle(
-        root,
-        resize(20, direction),
-        [Math.random() * direction[0], Math.random()])
-    });
+    const height = document.createAttribute('height');
+    height.value = container.offsetHeight;
+
+    const layer = document.createElement('canvas');
+    layer.setAttributeNode(width);
+    layer.setAttributeNode(height);
+
+    return layer;
 }
 
-function cursorPosition(canvas, event) {
-  const rect = canvas.getBoundingClientRect();
-  return [event.clientX - rect.left, event.clientY - rect.top];
+function seed(numberOfParticles, root) {
+    return Array(numberOfParticles)
+        .fill(null)
+        .map(() => {
+            const direction = unitVector();
+
+            return particle(
+                root,
+                resize(20, direction),
+                [Math.random() * direction[0], Math.random()])
+        });
+}
+
+function cursorPosition(element, event) {
+    const rect = element.getBoundingClientRect();
+    return [event.clientX - rect.left, event.clientY - rect.top];
 }
 
--- js/render.js
+++ js/render.js
@@ -1,39 +1,48 @@
-function render(context, particles, pixel) {
-  requestAnimationFrame(drawFrame.bind(null, preCalculate(particles)));
+function render(canvas, particles, pixel) {
+    const context = canvas.getContext('2d');
+    const animationData = preCalculate(particles);
+    console.info(animationData);
+    requestAnimationFrame(drawFrame.bind(null, animationData));
 
-  function drawFrame(frames) {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    function drawFrame(frames) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        frames.shift().forEach(coordinates => context.drawImage(pixel, coordinates[0], coordinates[1]));
 
-    frames
-      .shift()
-      .map(particle => particle.coordinates)
-      .forEach(coordinates => context.drawImage(pixel, coordinates[0], coordinates[1]));
-
-    if (frames.length > 0) {
-      requestAnimationFrame(drawFrame.bind(null, frames));
+        if (frames.length > 0) {
+            requestAnimationFrame(drawFrame.bind(null, frames));
+        } else {
+            canvas.remove();
+        }
     }
-  }
 
-  function preCalculate(particles, frameData = [particles]) {
-    const futureParticles = particles.map(move).filter(isInBounds);
-    frameData.push(futureParticles);
-    return (futureParticles.length > 0) ? preCalculate(futureParticles, frameData) : frameData;
+    function preCalculate(particles, animationData = []) {
+        if (particles.length === 0) {
+            return animationData;
+        } else {
+            const nextFrame = particles.map(move).filter(isInBounds);
+            return preCalculate(
+                nextFrame,
+                animationData.concat([
+                    nextFrame.map(particle => particle.coordinates)
+                ])
+            );
+        }
+    }
 
     function isInBounds(particle) {
-      return (particle.coordinates[0] > 0 && particle.coordinates[0] < context.canvas.width &&
-        particle.coordinates[1] > 0 && particle.coordinates[1] < context.canvas.height);
+        return (particle.coordinates[0] > 0 && particle.coordinates[0] < canvas.width &&
+            particle.coordinates[1] > 0 && particle.coordinates[1] < canvas.height);
     }
-  }
 }
 
 function createPixelImage(size, colour) {
-  const canvas = document.createElement("canvas");
-  canvas.width = size;
-  canvas.height = size;
+    const canvas = document.createElement("canvas");
+    canvas.width = size;
+    canvas.height = size;
 
-  const context = canvas.getContext("2d");
-  context.fillStyle = colour;
-  context.fillRect(0, 0, size, size);
+    const context = canvas.getContext("2d");
+    context.fillStyle = colour;
+    context.fillRect(0, 0, size, size);
 
-  return canvas;
+    return canvas;
 }
