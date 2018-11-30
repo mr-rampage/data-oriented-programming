575b5b64cf3e0636f10c02b849613cc1dac48f42
mr-rampage <readonly@che.wbac.ca>
2018-11-30 01:05:09 -0500

    refactor(externalizing configuration)

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
-const container = document.querySelector('#particleContainer');
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector('#particleContainer');
+    const pixelSize = parseInt(container.getAttribute('data-pixel-size'), 10);
+    const explosionSize = parseInt(container.getAttribute('data-explosion-size'), 10);
+    const maxVelocity = parseInt(container.getAttribute('data-max-velocity'), 10);
 
-container.addEventListener('click', (e) => {
-    const canvas = container.appendChild(particleLayer(container));
-    render(
-        canvas,
-        seed(500, cursorPosition(canvas, e)),
-        createPixelImage(2, randomColour())
-    );
+    container.addEventListener('click', (e) => {
+        const canvas = container.appendChild(particleLayer(container));
+        canvas.style.position = 'absolute';
+        render(
+            canvas,
+            seed(explosionSize, maxVelocity, cursorPosition(canvas, e)),
+            createPixelImage(pixelSize, randomColour())
+        );
+    });
 });
+
+function seed(numberOfParticles, maxVelocity, coordinates) {
+    return Array(numberOfParticles)
+        .fill(null)
+        .map(() => {
+            const direction = unitVector();
+
+            return particle(
+                coordinates,
+                resize(maxVelocity, direction),
+                [Math.random() * direction[0], Math.random()])
+        });
+}
 
 function particleLayer(container) {
     const width = document.createAttribute('width');
--- js/particle.js
+++ js/particle.js
@@ -2,16 +2,3 @@ function particle(coordinates, vector, acceleration) {
     return {coordinates, vector, acceleration};
 }
 
-function seed(numberOfParticles, coordinates) {
-    return Array(numberOfParticles)
-        .fill(null)
-        .map(() => {
-            const direction = unitVector();
-
-            return particle(
-                coordinates,
-                resize(20, direction),
-                [Math.random() * direction[0], Math.random()])
-        });
-}
-
